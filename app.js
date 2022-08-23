1fd8febb7c2ac8322a32a8c3df360da9e1421a15
Kai-Al <Kai.albornoz@udea.edu.co>
2022-08-23 18:52:31 -0500

    Añadiendo color al precio.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 //Se crea el webSocket con el servidor de binance.
 let webSocket = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
 
+//Esta variable
+let stockSymbol = document.getElementById('stockSymbol');
+
+//Esta variable va a guardar el último precio de la accion.
+let lastPrice = null;
+
 //Este es el parrafo que se actualiza con el precio de la accion.
 let stockPrice = document.getElementById('stockPrice');
 
@@ -17,4 +23,7 @@ webSocket.onmessage = function(event) {
     //Acá se ingresa el precio de la accion en el parrafo
     // y se ponen 2 decimales.
     stockPrice.innerHTML = parseFloat(stockObject.p).toFixed(2);
+    stockSymbol.innerText = stockObject.s;
+    stockPrice.style.color = !lastPrice ? 'black' : lastPrice > stockObject.p ? 'red' : 'green';
+    lastPrice = stockObject.p;
 }
